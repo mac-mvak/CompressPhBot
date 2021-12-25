a8b0cfcb6d2ab493616de68689122f028b787ff3
Mac_Nikak <maximusev1290@gmail.com>
2021-12-25 17:30:04 +1000

    Change invitation

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(os.getenv('TELEGRAM_TOKEN'))
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Hello, how are you doing?")
 
 
 @bot.message_handler(func=lambda message: True)
