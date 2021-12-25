ec82271e682f5c5f292432ab767d2880c0628e32
mac-mvak <maximusev1290@gmail.com>
2021-12-25 20:51:31 +1000

    Working with photos Second version.

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ bot = telebot.TeleBot(os.getenv('TELEGRAM_TOKEN'))
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello it's compress bot. Send me photo and I will compress it.")
+    bot.reply_to(message, "Hello it's compress bot."
+                          " Send me photo and I will compress it.")
 
 
 @bot.message_handler(content_types=['photo'])
