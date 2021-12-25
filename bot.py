df368dee1a50a9188422fea995a14e6724892406
Mac_Nikak <maximusev1290@gmail.com>
2021-12-25 19:44:54 +1000

    Now it's the best bot!

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(os.getenv('TELEGRAM_TOKEN'))
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Worst bot ever seen.")
+    bot.reply_to(message, "Best bot ever seen.")
 
 
 @bot.message_handler(func=lambda message: True)
